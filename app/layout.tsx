39272613dc0be9da8eba9aa9a4fd53f4fe199f6e
Cristhian Almeida <3correntes@gmail.com>
2024-02-16 19:37:46 -0400

    add: container component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import NavBar from './components/nav/NavBar';
+import Footer from './components/footer/Footer';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -13,7 +15,7 @@ export const metadata: Metadata = {
     url: 'https://janelastore.vercel.app/',
     siteName: 'janelaStore',
     images: [{ url: 'https://janelastore.vercel.app/logo.png' }],
-    locale: 'pt_BR',
+    locale: 'pt-BR',
     type: 'website'
   }
 };
@@ -25,7 +27,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={poppins.className}>{children}</body>
+      <body className={`${poppins.className} text-slate-700`}>
+        <div className="flex flex-col min-h-screen">
+          <NavBar />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main>janelaStore</main>;
+  return <div>janelaStore</div>;
 }
